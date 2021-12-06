86870aea320d3aa2b820095b2187e260dfdba815
Mael Police <police@et.esiea.fr>
2021-12-06 23:39:53 +0100

    Rebuild of the tests in LauncherTest

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -2,33 +2,28 @@ package fr.lernejo.navy_battle;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LauncherTest {
-    boolean CheckArg(String args) {
-        return (args.length() == 1);
+    boolean CheckArg(String... args) {
+        return (args.length == 1);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"arg1"})
-    void test_CheckArg_one_true(String args) {
-        assertTrue(CheckArg(args));
-    }
+
+    void test_CheckArg_one_true(String args) {assertTrue(CheckArg(args));}
 
     @ParameterizedTest
     @EmptySource
-    @NullSource
-    @ValueSource(strings = {""})
-    void test_CheckArg_empty_false(String args) {
-        assertFalse(CheckArg(args));
-    }
+    @ValueSource(strings = {"arg0"})
+    void test_CheckArg_empty_false(String args) { assertFalse(CheckArg()); }
 
     @ParameterizedTest
-    @ValueSource(strings = {"arg1", "arg2"})
+    @ValueSource(strings = {"arg2"})
     void test_CheckArg_more_false(String args) {
-        assertFalse(CheckArg(args));
+        assertFalse(CheckArg(args, args));
     }
 }
